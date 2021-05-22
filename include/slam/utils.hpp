525ebb14d39ba4f63ca8eaeb8e83198cae83d1ef
dmn-sjk <sojka.damian9@gmail.com>
2021-05-22 19:32:46 +0200

    icp kinda working

---
+++ include/slam/utils.hpp
@@ -0,0 +1,31 @@
+#include <Eigen/Dense>
+
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/TransformStamped.h>
+
+namespace slam{
+
+    static geometry_msgs::TransformStamped matrix_to_trans(const ros::Time &stamp, const Eigen::Matrix4f& matrix, const std::string& frame_id, const std::string& child_frame_id){
+        Eigen::Quaternionf quat(matrix.block<3, 3>(0, 0));
+        quat.normalize();
+        geometry_msgs::Quaternion rotation;
+        rotation.x = quat.x();
+        rotation.y = quat.y();
+        rotation.z = quat.z();
+        rotation.w = quat.w();
+
+        geometry_msgs::TransformStamped transform;
+        transform.header.stamp = stamp;
+        transform.header.frame_id = frame_id;
+        transform.child_frame_id = child_frame_id;
+        transform.transform.translation.x = matrix(0, 3);
+        transform.transform.translation.y = matrix(1, 3);
+        transform.transform.translation.z = matrix(2, 3);
+        transform.transform.rotation = rotation;
+
+        return transform;
+    }
+
+}
--- src/slam.cpp
+++ src/slam.cpp
@@ -5,9 +5,12 @@
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/point_types.h>
 #include <pcl/registration/icp.h>
+#include <pcl/registration/gicp.h>
 #include <iostream>
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
+
+#include <slam/utils.hpp>
 
 
 class Slam{
@@ -16,6 +19,9 @@ private:
     ros::NodeHandle nh;
     ros::Subscriber cloud_sub;
     ros::Publisher cloud_pub;
+    ros::Publisher aligned_cloud_pub;
+    tf::TransformBroadcaster tf_br;
+    tf::TransformListener tf_ls;
 
     // frames
     std::string odom_frame_id = "lidar";
@@ -23,6 +29,7 @@ private:
     std::string cloud_frame_id = "cloud";
     
     // odometry
+    pcl::Registration<pcl::PointXYZI, pcl::PointXYZI>::Ptr registration;
     ros::Time prev_time;
     Eigen::Matrix4f prev_trans;
     Eigen::Matrix4f keyframe_pose;
@@ -30,6 +37,11 @@ private:
     pcl::PointCloud<pcl::PointXYZI>::ConstPtr keyframe;
     pcl::PointCloud<pcl::PointXYZI>::Ptr prev_cloud = pcl::PointCloud<pcl::PointXYZI>::Ptr(new pcl::PointCloud <pcl::PointXYZI>);
 
+    // The minimum tranlational distance and rotation angle between keyframes.
+    // If this value is zero, frames are always compared with the previous frame
+    double keyframe_delta_trans = 0.25;
+    double keyframe_delta_angle = 0.15;
+    double keyframe_delta_time = 1.0;
 
 
 public:
@@ -38,43 +50,30 @@ public:
         //whole cloud
 		cloud_sub = nh.subscribe<sensor_msgs::PointCloud2>("/cloud", 2, &Slam::cloud_handler, this);
 
-		cloud_pub = nh.advertise<sensor_msgs::PointCloud2>("/static_cloud", 1);
+		aligned_cloud_pub = nh.advertise<sensor_msgs::PointCloud2>("/aligned_cloud", 1);
         
+        registration = choose_registration("icp");
 	}
-    
 
-    void update_pose(const Eigen::Matrix4f &trans_matrix){
-        static tf::TransformBroadcaster br;
-        static tf::TransformListener listener;
-
-        tf::StampedTransform transform;
-        tf::Quaternion tf_prev_quat;
-        tf::Vector3 tf_prev_trans;
-
-        Eigen::Quaterniond eigen_quat(trans_matrix.block<3,3>(0,0).cast<double>());
-        Eigen::Vector3d eigen_trans(trans_matrix.block<3,1>(0,3).cast<double>());
-
-        tf::Quaternion tf_quat(eigen_quat.x(), eigen_quat.y(), eigen_quat.z(), eigen_quat.w());
-        tf::Vector3 tf_trans(eigen_trans(0), eigen_trans(1), eigen_trans(2));
-
-        try{
-            listener.lookupTransform("cloud", "lidar", ros::Time(0), transform);
+    boost::shared_ptr<pcl::Registration<pcl::PointXYZI, pcl::PointXYZI>> choose_registration(std::string reg){
+        if (reg == "icp"){
+            boost::shared_ptr<pcl::IterativeClosestPoint<pcl::PointXYZI, pcl::PointXYZI>> icp(new pcl::IterativeClosestPoint<pcl::PointXYZI, pcl::PointXYZI>());
+            icp->setTransformationEpsilon(0.01);
+            icp->setMaximumIterations(64);
+            icp->setMaxCorrespondenceDistance(2.5);
+            icp->setUseReciprocalCorrespondences(false);
+            return icp;
         }
-        catch (tf::TransformException ex){
-            ROS_ERROR("%s", ex.what());
+        else if (reg == "gicp"){
+            boost::shared_ptr<pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZI, pcl::PointXYZI>> gicp(new pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZI, pcl::PointXYZI>());
+            gicp->setTransformationEpsilon(0.01);
+            gicp->setMaximumIterations(64);
+            gicp->setUseReciprocalCorrespondences(false);
+            gicp->setMaxCorrespondenceDistance(2.5);
+            gicp->setCorrespondenceRandomness(20);
+            gicp->setMaximumOptimizerIterations(20);
+            return gicp;
         }
-
-        tf_prev_trans = transform.getOrigin();
-        tf_prev_quat = transform.getRotation();
-
-        tf::Vector3 tf_fin_trans(tf_prev_trans.x() + tf_trans.x(), tf_prev_trans.y() + tf_trans.y(), tf_prev_trans.z() + tf_trans.z());
-        tf::Quaternion tf_fin_quat(tf_prev_quat.x() + tf_quat.x(), tf_prev_quat.y() + tf_quat.y(), tf_prev_quat.z() + tf_quat.z(), tf_prev_quat.w() + tf_quat.w());
-        
-        std::cout<<"\n"<<tf_fin_quat.x()<<", "<<tf_fin_quat.y()<<", "<<tf_fin_quat.z()<<", "<<tf_fin_quat.w();
-
-        transform.setOrigin(tf_fin_trans);
-        transform.setRotation(tf_fin_quat);
-        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "cloud", "lidar"));
     }
 
     pcl::PointCloud<pcl::PointXYZI>::Ptr downsample(const pcl::PointCloud<pcl::PointXYZI>::ConstPtr &cloud){
@@ -88,28 +87,58 @@ public:
     }
 
     Eigen::Matrix4f match(const ros::Time &stamp, const pcl::PointCloud<pcl::PointXYZI>::ConstPtr &cloud){
-
-        pcl::IterativeClosestPoint<pcl::PointXYZI, pcl::PointXYZI> icp;
-        icp.setInputSource(cloud);
-        icp.setInputTarget(keyframe);
+        
+        if(!keyframe) {
+            prev_time = ros::Time();
+            prev_trans.setIdentity();
+            keyframe_pose.setIdentity();
+            keyframe_stamp = stamp;
+            keyframe = downsample(cloud);
+            registration->setInputTarget(keyframe);
+            return Eigen::Matrix4f::Identity();
+        }
+        
+        
+        registration->setInputSource(cloud);
+        registration->setInputTarget(keyframe);
         
         Eigen::Isometry3f msf_delta = Eigen::Isometry3f::Identity();
         
         pcl::PointCloud<pcl::PointXYZI>::Ptr aligned(new pcl::PointCloud <pcl::PointXYZI>());
-        icp.align(*aligned, prev_trans * msf_delta.matrix());
+        registration->align(*aligned, prev_trans * msf_delta.matrix());
 
-        if(!icp.hasConverged()){
+        if(!registration->hasConverged()){
             ROS_INFO("Not converged!!!");
             return keyframe_pose * prev_trans;
         }
 
-        Eigen::Matrix4f transform = icp.getFinalTransformation();
+        Eigen::Matrix4f transform = registration->getFinalTransformation();
         Eigen::Matrix4f odom = keyframe_pose * transform;
 
         prev_time = stamp;
         prev_trans = transform;
 
+        auto keyframe_trans = slam::matrix_to_trans(stamp, keyframe_pose, world_frame_id, odom_frame_id);
+        tf_br.sendTransform(keyframe_trans);
 
+        double delta_trans = transform.block<3, 1>(0, 3).norm();
+        double delta_angle = std::acos(Eigen::Quaternionf(transform.block<3, 3>(0, 0)).w());
+        double delta_time = (stamp - keyframe_stamp).toSec();
+        if(delta_trans > keyframe_delta_trans || delta_angle > keyframe_delta_angle || delta_time > keyframe_delta_time) {
+            keyframe = cloud;
+            registration->setInputTarget(keyframe);
+
+            keyframe_pose = odom;
+            keyframe_stamp = stamp;
+            prev_time = stamp;
+            prev_trans.setIdentity();
+        }
+
+        pcl::transformPointCloud (*cloud, *aligned, odom);
+        aligned->header.frame_id=odom_frame_id;
+        aligned_cloud_pub.publish(aligned);
+
+        return odom;
     }
 
 	void cloud_handler(const sensor_msgs::PointCloud2ConstPtr &cloud_msg){
@@ -120,15 +149,14 @@ public:
 
         auto cloud_filtered = downsample(input_cloud);
 
-        if (prev_cloud->points.size() == 0){
-            prev_cloud = cloud_filtered;
-        }
+        // if (prev_cloud->points.size() == 0){
+        //     prev_cloud = cloud_filtered;
+        // }
 
         //pcl::PointCloud<pcl::PointXYZI>::Ptr static_cloud(new pcl::PointCloud <pcl::PointXYZI>);
 
         auto transform = match(cloud_msg->header.stamp, cloud_filtered);
 
-        update_pose(transform);
         //pcl::transformPointCloud (*input_cloud, *static_cloud, transform);
 
         //sensor_msgs::PointCloud2 cloud_out;
@@ -143,7 +171,7 @@ public:
         // Publish message 
         //cloud_pub.publish(cloud_out);
 
-        prev_cloud = input_cloud;
+        //prev_cloud = input_cloud;
 	}
 
 	float euclidan_dist(float x, float y, float z){
