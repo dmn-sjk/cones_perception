0f72fdefeb3340d718ec19a23a7a570ce775fb58
dmn-sjk <sojka.damian9@gmail.com>
2021-05-31 15:05:46 +0200

    cleaned cone detection

--- src/cone_detection.cpp
+++ src/cone_detection.cpp
@@ -29,7 +29,6 @@ public:
 
 	void cloud_handler(const sensor_msgs::PointCloud2ConstPtr &cloud_msg){
 
-		// Converting ROS message to point cloud
         pcl::PointCloud<pcl::PointXYZI>::Ptr input_cloud(new pcl::PointCloud <pcl::PointXYZI>);
         pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered(new pcl::PointCloud <pcl::PointXYZI>);
 
@@ -37,31 +36,16 @@ public:
 
 		int cloud_size = input_cloud->points.size();
 
-        ////
-        //arr_size = *(&arr + 1) - arr;
-
+		// Deleting points too far away, too close and limiting horizontal FoV
         input_cloud->points.erase(std::remove_if(input_cloud->points.begin(), input_cloud->points.end(),
             [&](pcl::PointXYZI p){return p.z < -0.5 or this->euclidan_dist(p.x, p.y, p.z) > 4 or
 			this->euclidan_dist(p.x, p.y, p.z) < 0.7 or -10 * M_PI / 18 >= atan2(p.y, p.x) or
 			atan2(p.y, p.x) >= 10 * M_PI / 18;}), input_cloud->points.end());
 
-        pcl::VoxelGrid<pcl::PointXYZI> vg;
-        vg.setInputCloud(input_cloud);
-        vg.setLeafSize(0.04f, 0.04f, 0.05f);
-        vg.filter(*cloud_filtered);
-
-
-  		pcl::search::KdTree<pcl::PointXYZI>::Ptr kdtree (new pcl::search::KdTree<pcl::PointXYZI>);
-  		kdtree->setInputCloud (cloud_filtered);
+		cloud_filtered = downsample(input_cloud);
 
   		std::vector<pcl::PointIndices> cluster_indices;
-	  	pcl::EuclideanClusterExtraction<pcl::PointXYZI> ec;
-	  	ec.setClusterTolerance (sqrt(pow(this->cone_height, 2) + pow(this->cone_width / 2, 2))); // 2cm
-	  	ec.setMinClusterSize (3);
-	  	ec.setMaxClusterSize (50);
-	  	ec.setSearchMethod (kdtree);
-	  	ec.setInputCloud (cloud_filtered);
-	  	ec.extract (cluster_indices);
+		cluster_indices = euclidan_cluster(cloud_filtered);
 
 	  	pcl::PointCloud<pcl::PointXYZI>::Ptr centroid_cloud (new pcl::PointCloud<pcl::PointXYZI>);
 	  	int j = 0;
@@ -71,7 +55,6 @@ public:
 		{
 		    pcl::PointXYZI p;
 		    for (const auto& idx : it->indices){
-		    	//cloud_cluster->push_back ((*cloud_filtered)[idx]); //*
 		    	x = (*cloud_filtered)[idx].x + x;
 		    	y = (*cloud_filtered)[idx].y + y;
 		    	j++;
@@ -85,26 +68,43 @@ public:
 		    j = 0;
 			x = 0.0;
 			y = 0.0;
-		    //cloud_cluster->width = cloud_cluster->size ();
-		    //cloud_cluster->height = 1;
-		    //cloud_cluster->is_dense = true;
 		}
-        ////
-
-        //input_cloud->points.resize(cloud_size);
 
         sensor_msgs::PointCloud2 cones_cloud;
-
-        // ROS_INFO("distance: [%f]", this->euclidan_dist(this->cone_height, 2.0, this->cone_width));
         
-        // Convert created point cloud to message
         pcl::toROSMsg(*centroid_cloud, cones_cloud);
         
-        cones_cloud.header.frame_id = "cloud";
+		cones_cloud.header = cloud_msg->header;
+		cones_cloud.fields = cloud_msg->fields;
 
-        // Publish message 
         cones_pub.publish(cones_cloud);
 	}
+
+	std::vector<pcl::PointIndices> euclidan_cluster(const pcl::PointCloud<pcl::PointXYZI>::ConstPtr &cloud){
+		pcl::search::KdTree<pcl::PointXYZI>::Ptr kdtree (new pcl::search::KdTree<pcl::PointXYZI>);
+  		kdtree->setInputCloud (cloud);
+
+        std::vector<pcl::PointIndices> cluster_indices;
+	  	pcl::EuclideanClusterExtraction<pcl::PointXYZI> ec;
+	  	ec.setClusterTolerance (sqrt(pow(this->cone_height, 2) + pow(this->cone_width / 2, 2))); 
+	  	ec.setMinClusterSize (3);
+	  	ec.setMaxClusterSize (50);
+	  	ec.setSearchMethod (kdtree);
+	  	ec.setInputCloud (cloud);
+	  	ec.extract (cluster_indices);
+
+        return cluster_indices;
+    }
+
+	pcl::PointCloud<pcl::PointXYZI>::Ptr downsample(const pcl::PointCloud<pcl::PointXYZI>::ConstPtr &cloud){
+        pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered(new pcl::PointCloud <pcl::PointXYZI>);
+        pcl::VoxelGrid<pcl::PointXYZI> vg;
+        vg.setInputCloud(cloud);
+        vg.setLeafSize(0.04f, 0.04f, 0.05f);
+        vg.filter(*cloud_filtered);
+
+        return cloud_filtered;
+    }
 
 	float euclidan_dist(float x, float y, float z){
 		return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
